8e6e4723dc4597d62c702466081448f6a3bff044
higorgoulart <higorgoulart@hotmail.com>
2023-08-23 19:23:19 -0300

    init frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+        <h1 className="text-primary">agenday</h1>
+        <span className="loading loading-infinity loading-lg"></span>
+    </>
+  )
+}
+
+export default App
