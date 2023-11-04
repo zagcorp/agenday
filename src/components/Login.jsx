e92944f24cefff5c252a32f12b2bf71c2fc5cdd4
GabrielGuinzani <gferreiraguinzani@gmail.com>
2023-11-04 16:28:35 -0300

    Adicionados botões ao componente de Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,3 +1,5 @@
+
+
 export function Login() {
   return (
     <div className="container xl:py-20 lg:p-14 md:p-10 p-6 flex flex-col justify-center align-middle lg:basis-4/12 sm:rounded-r-xl text-center bg-base-100">
@@ -13,6 +15,10 @@ export function Login() {
           placeholder="Senha"
           className="input input-bordered input-secondary"
         />
+        <div className="flex justify-between">
+        <button className=" text-accent flex-initial w-64 flex-1 mr-5 hover:underline">Cadastrar-se</button>
+        <button className="btn btn-secondary flex-3 ml-5">Entrar</button>
+        </div>
       </div>
     </div>
   );
